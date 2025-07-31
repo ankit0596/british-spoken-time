f37033ca9fbc34b7c1bb64f56472e3f1e7cf42c5
ankitsrivastava <ankit050603@gmail.com>
2025-08-01 02:21:20 +0530

    Refactor SpokenTimeResponseDto to use lombok

--- src/main/java/com/example/britishtime/dto/SpokenTimeResponseDto.java
+++ src/main/java/com/example/britishtime/dto/SpokenTimeResponseDto.java
@@ -1,29 +1,13 @@
 package com.example.britishtime.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
 public class SpokenTimeResponseDto {
     private String input;
     private String spokenTime;
-
-    public SpokenTimeResponseDto(String input, String spoken) {
-        this.input = input;
-        this.spokenTime = spoken;
-    }
-
-    public String getInput() {
-        return input;
-    }
-
-    public void setInput(String input) {
-        this.input = input;
-    }
-
-    public String getSpokenTime() {
-        return spokenTime;
-    }
-
-    public void setSpokenTime(String spokenTime) {
-        this.spokenTime = spokenTime;
-    }
-
-
 }
