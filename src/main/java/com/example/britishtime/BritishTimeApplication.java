6dbbacaaa44494eb77d73dc711135e4ab4210a30
ankitsrivastava <ankit050603@gmail.com>
2025-07-31 23:06:08 +0530

    initial commit

---
+++ src/main/java/com/example/britishtime/BritishTimeApplication.java
@@ -0,0 +1,13 @@
+package com.example.britishtime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BritishTimeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BritishTimeApplication.class, args);
+	}
+
+}
